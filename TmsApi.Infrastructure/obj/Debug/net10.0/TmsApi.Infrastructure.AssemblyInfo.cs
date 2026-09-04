f176c2883e4b19a24feb57fc286dc46ac05138db
Aman@3823 <amanuelsewnetgmail.com>
2026-09-04 18:58:05 +0300

    chore: Update assembly informational version and cache files for all projects

--- TmsApi.Infrastructure/obj/Debug/net10.0/TmsApi.Infrastructure.AssemblyInfo.cs
+++ TmsApi.Infrastructure/obj/Debug/net10.0/TmsApi.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TmsApi.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ddfcab8455b958fc84419c52fc858444d47a153")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b1a926921fb474e1b0bfa39a02a4ea9896d4a6d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TmsApi.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TmsApi.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
